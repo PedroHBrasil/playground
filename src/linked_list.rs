24edadf080e2079909ec468cc46ac30de2a2dd07
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-07-22 17:05:02 -0300

    implemented get value methods of linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,7 +1,43 @@
+use std::error::Error;
+use self::node::NodeSingle;
+
 mod node;
 
 /// Represents a single-linked list
 pub struct LinkedList<'a, T> {
     // First node of the linked list
-    head: &'a node::NodeSingle<'a, T>
+    head: &'a NodeSingle<'a, T>
+}
+
+impl<'a, T> LinkedList<'a, T> {
+
+    /// Gets value of first node
+    pub fn get_first_value(&self) -> &T {
+        self.head.get_data()
+    }
+
+    /// Gets value of last node
+    pub fn get_last_value(&self) -> &T {
+        // Sweeps nodes until the last one (.next is None) is found
+        let mut cur_node = self.head;
+        while let Ok(next_node) = cur_node.get_next() {
+            cur_node = next_node;
+        }
+
+        cur_node.get_data()
+    }
+
+    /// Gets value of node at index i
+    pub fn get_value_at(&self, i: u32) -> Result<&T, Box<dyn Error>> {
+        // Sweeps nodes until the target index. Panics if the index is bigger than the number of nodes.
+        let mut cur_node = self.head;
+        let mut counter: u32 = 0;
+        while counter != i {
+            cur_node = cur_node.get_next()?;
+            counter += 1;
+        }
+
+        Ok(cur_node.get_data())
+    }
+
 }
--- src/linked_list/node.rs
+++ src/linked_list/node.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 
 /// Represents a node of a single linked list
 pub struct NodeSingle<'a, T> {
@@ -9,19 +11,23 @@ pub struct NodeSingle<'a, T> {
 
 impl<'a, T> NodeSingle<'a, T> {
     /// Gets the node's data
-    fn get_data(&self) -> &T {
+    pub fn get_data(&self) -> &T {
         &self.data
     }
     /// Sets the node's data
-    fn set_data(&mut self, new_data: T) -> () {
+    pub fn set_data(&mut self, new_data: T) -> () {
         self.data = new_data;
     }
     /// Gets the next node
-    fn get_next(&self) -> Option<&Self> {
-        self.next
+    pub fn get_next(&self) -> Result<&Self, Box<dyn Error>> {
+        if let Some(next_node) = self.next {
+            Ok(next_node)
+        } else {
+            Err("No next node for current node.".to_string().into())
+        }
     }
     /// Sets the next node
-    fn set_next(&mut self, new_next: Option<&'a NodeSingle<'a, T>>) -> () {
+    pub fn set_next(&mut self, new_next: Option<&'a NodeSingle<'a, T>>) -> () {
         self.next = new_next;
     }
 }

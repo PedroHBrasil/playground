405cf6a230282e30f4ee49d3e17a72dbe2262dd5
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-07-22 19:02:22 -0300

    split get methods into node and value

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -10,25 +10,22 @@ pub struct LinkedList<'a, T> {
 }
 
 impl<'a, T> LinkedList<'a, T> {
-
-    /// Gets value of first node
-    pub fn get_first_value(&self) -> &T {
-        self.head.get_data()
+    /// Gets first node
+    pub fn get_first_node(&self) -> &NodeSingle<T> {
+        self.head
     }
-
     /// Gets value of last node
-    pub fn get_last_value(&self) -> &T {
+    pub fn get_last_node(&self) -> &NodeSingle<T> {
         // Sweeps nodes until the last one (.next is None) is found
         let mut cur_node = self.head;
         while let Ok(next_node) = cur_node.get_next() {
             cur_node = next_node;
         }
 
-        cur_node.get_data()
+        cur_node
     }
-
     /// Gets value of node at index i
-    pub fn get_value_at(&self, i: u32) -> Result<&T, Box<dyn Error>> {
+    pub fn get_node_at(&self, i: u32) -> Result<&NodeSingle<T>, Box<dyn Error>> {
         // Sweeps nodes until the target index. Panics if the index is bigger than the number of nodes.
         let mut cur_node = self.head;
         let mut counter: u32 = 0;
@@ -37,7 +34,18 @@ impl<'a, T> LinkedList<'a, T> {
             counter += 1;
         }
 
-        Ok(cur_node.get_data())
+        Ok(cur_node)
     }
-
+    /// Gets value of first node
+    pub fn get_first_value(&self) -> &T {
+        self.get_first_node().get_data()
+    }
+    /// Gets value of last node
+    pub fn get_last_value(&self) -> &T {
+        self.get_last_node().get_data()
+    }
+    /// Gets value of node at index i
+    pub fn get_value_at(&self, i: u32) -> Result<&T, Box<dyn Error>> {
+        Ok(self.get_node_at(i)?.get_data())
+    }
 }

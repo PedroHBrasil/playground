c3c302826a9fc155a2fb2b70c6edf984c23c1664
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-07-24 14:41:37 -0300

    refactored linked list node

--- src/linked_list/node.rs
+++ src/linked_list/node.rs
@@ -11,6 +11,12 @@ pub struct NodeSingle<'a, T> {
 }
 
 impl<'a, T> NodeSingle<'a, T> {
+    pub fn new(data: T, next: Option<&'a NodeSingle<'a, T>>) -> Self {
+        Self {
+            data,
+            next,
+        }
+    }
     /// Gets the node's data
     pub fn get_data(&self) -> &T {
         &self.data
@@ -24,12 +30,12 @@ impl<'a, T> NodeSingle<'a, T> {
         if let Some(next_node) = self.next {
             Ok(next_node)
         } else {
-            Err(String::from("No next node for current node.").into())
+            Err("No next node for current node.".into())
         }
     }
     /// Sets the next node
-    pub fn set_next(&mut self, new_next: &'a NodeSingle<'a, T>) -> () {
-        self.next = Some(new_next);
+    pub fn set_next(&self, new_next: Option<&'a NodeSingle<'a, T>>) -> () {
+        self.next = new_next;
     }
 }
 
@@ -41,10 +47,7 @@ mod tests {
     fn get_data_node_single_i32() {
         // Data
         let data = 0;
-        let node = NodeSingle {
-            data,
-            next: None,
-        };
+        let node = NodeSingle::new(data, None);
         // Run
         let result = *node.get_data();
         // Assert
@@ -55,10 +58,7 @@ mod tests {
     fn get_data_node_single_char() {
         // Data
         let data = 'a';
-        let node = NodeSingle {
-            data,
-            next: None,
-        };
+        let node = NodeSingle::new(data, None);
         // Run
         let result = *node.get_data();
         // Assert
@@ -69,10 +69,7 @@ mod tests {
     fn set_data_node_single_i32() {
         // Data
         let data = 0;
-        let mut node = NodeSingle {
-            data,
-            next: None,
-        };
+        let mut node = NodeSingle::new(data, None);
         let new_data = 1;
         // Run
         node.set_data(new_data);
@@ -85,10 +82,7 @@ mod tests {
     fn set_data_node_single_char() {
         // Data
         let data = 'a';
-        let mut node = NodeSingle {
-            data,
-            next: None,
-        };
+        let mut node = NodeSingle::new(data, None);
         let new_data = 'b';
         // Run
         node.set_data(new_data);
@@ -101,10 +95,7 @@ mod tests {
     fn get_next_node_single_none() {
         // Data
         let data = 0;
-        let node = NodeSingle {
-            data: data,
-            next: None,
-        };
+        let node = NodeSingle::new(data, None);
         // Run
         let result = node.get_next();
         // Assert
@@ -115,15 +106,9 @@ mod tests {
     fn get_next_node_single_i32() {
         // Data
         let data1 = 1;
-        let node1 = NodeSingle {
-            data: data1,
-            next: None,
-        };
         let data0 = 0;
-        let node0 = NodeSingle {
-            data: data0,
-            next: Some(&node1),
-        };
+        let mut node1 = NodeSingle::new(data1, None);
+        let node0 = NodeSingle::new(data0, Some(&mut node1));
         // Run
         let result = node0.get_next().unwrap();
         // Assert
@@ -134,17 +119,11 @@ mod tests {
     fn set_next_node_single_i32() {
         // Data
         let data0 = 0;
-        let mut node0 = NodeSingle {
-            data: data0,
-            next: None,
-        };
         let data1 = 1;
-        let node1 = NodeSingle {
-            data: data1,
-            next: None,
-        };
+        let mut node0 = NodeSingle::new(data0, None);
+        let mut node1 = NodeSingle::new(data1, None);
         // Run
-        node0.set_next(&node1);
+        node0.set_next(Some(&mut node1));
         let result = node0.next.unwrap();
         // Assert
         assert_eq!(*result, node1);
